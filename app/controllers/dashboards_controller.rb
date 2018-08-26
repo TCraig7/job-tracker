29378baf748b57b24527481a523d90d40722df67
Mike McKee <mikecm@gmail.com>
2018-08-26 14:47:05 -0600

    Add user feature spec for companies by average interest

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def index
     @jobs_by_location = Job.count_by_location
+    @companies_by_interest = Company.companies_by_interest
   end
 end
--- spec/features/dashboard/user_sees_a_dashboard_spec.rb
+++ spec/features/dashboard/user_sees_a_dashboard_spec.rb
@@ -33,6 +33,31 @@ describe 'A user sees a job tracker dashboard' do
     end
   end
   scenario 'user sees top 3 companies by interest' do
-    
+    company1 = Company.create!(name: 'ESPN')
+    company2 = Company.create!(name: 'Google')
+    company3 = Company.create!(name: 'Twitter')
+    company4 = Company.create!(name: 'GoDaddy')
+    category = Category.create!(name: 'Something')
+    job1 = company1.jobs.create!(title: 'Dev', description: 'None', level_of_interest: 4, city: 'Denver', category_id: category.id)
+    job2 = company1.jobs.create!(title: 'Dev', description: 'None', level_of_interest: 5, city: 'Denver', category_id: category.id)
+    job3 = company2.jobs.create!(title: 'Dev', description: 'None', level_of_interest: 3, city: 'Denver', category_id: category.id)
+    job4 = company2.jobs.create!(title: 'Dev', description: 'None', level_of_interest: 4, city: 'Boulder', category_id: category.id)
+    job5 = company2.jobs.create!(title: 'Dev', description: 'None', level_of_interest: 5, city: 'Boulder', category_id: category.id)
+    job6 = company3.jobs.create!(title: 'Dev', description: 'None', level_of_interest: 3, city: 'Reno', category_id: category.id)
+    job7 = company3.jobs.create!(title: 'Dev', description: 'None', level_of_interest: 1, city: 'Reno', category_id: category.id)
+    job8 = company3.jobs.create!(title: 'Dev', description: 'None', level_of_interest: 5, city: 'Atlanta', category_id: category.id)
+    job9 = company4.jobs.create!(title: 'Dev', description: 'None', level_of_interest: 2, city: 'Portland', category_id: category.id)
+
+    visit dashboard_path
+
+    within("#job-interest-by-company-#{company1.id}") do
+      expect(page).to have_content(4.5)
+    end
+    within("#job-interest-by-company-#{company2.id}") do
+      expect(page).to have_content(4)
+    end
+    within("#job-interest-by-company-#{company3.id}") do
+      expect(page).to have_content(3)
+    end
   end
 end
