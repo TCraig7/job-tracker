45690d676d5da878618ef2ad1176869e726c28d6
Mike McKee <mikecm@gmail.com>
2018-08-27 18:44:01 -0600

    Adds styling to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,6 @@ class DashboardController < ApplicationController
     @jobs_by_location = Job.count_by_location
     @companies_by_interest = Company.companies_by_interest
     @count_by_interest = Job.count_by_interest
+    @header = 'Job Tracker Dashboard'
   end
 end
